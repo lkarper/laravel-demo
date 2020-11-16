9adc9cc5c55921ff024fe3354d1262e4ece47cbe
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-16 14:04:44 -0500

    Add form validation to create and edit views

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -18,19 +18,19 @@
             <div class="field">
                 <label for="title" class="label">Title</label>
                 <div class="control">
-                    <input type="text" class="input" name="title" id="title" value="{{ $article->title }}">
+                    <input type="text" class="input" name="title" id="title" value="{{ $article->title }}" required>
                 </div>
             </div>
             <div class="field">
                 <label for="excerpt" class="label">Excerpt</label>
                 <div class="control">
-                    <textarea class="textarea" name="excerpt" id="excerpt">{{ $article->excerpt }}</textarea>
+                    <textarea class="textarea" name="excerpt" id="excerpt" required>{{ $article->excerpt }}</textarea>
                 </div>
             </div>
             <div class="field">
                 <label for="body" class="label">Body</label>
                 <div class="control">
-                    <textarea class="textarea" name="body" id="body">{{ $article->body }}</textarea>
+                    <textarea class="textarea" name="body" id="body" required>{{ $article->body }}</textarea>
                 </div>
             </div>
             <div class="field is-grouped">
